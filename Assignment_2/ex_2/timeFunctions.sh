7327c8c20850b3ced55fd6a887cb72cfabde2b49
Makkish <marekv@kth.se>
2021-11-17 23:35:49 +0100

    trash files

--- Assignment_2/ex_2/timeFunctions.sh
+++
@@ -1,9 +0,0 @@
-## declare an array variable
-declare -a arr=("100" "1000" "10000" "100000" "1000000" "10000000" "100000000" "1000000000")
-
-echo -e "CPU\\tGPU\\tn"
-## now loop through the above array
-for i in "${arr[@]}"
-do
-   ./exercise_2_timer "$i"
-done
